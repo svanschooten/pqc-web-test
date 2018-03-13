764c60f5eb3548f6368bb0c9f69b4877808b36c3
Stijn van Schooten <stijn.vanschooten@onior.com>
2018-03-13 10:38:13 +0100

    aes key generation test

--- aes-test.js
+++ aes-test.js
@@ -9,3 +9,10 @@ function decryptAES() {
             iv: iv
         }).toString(CryptoJS.enc.Utf8);
 }
+
+function genkeyAES() {
+    let pc = document.getElementById("aes_pw_genkey").value;
+    let iv = document.getElementById("aes_iv_genkey").value;
+    document.getElementById("aes_iv").value = CryptoJS.enc.Utf8.parse(iv).toString(CryptoJS.enc.Hex);
+    document.getElementById("aes_key").value = CryptoJS.PBKDF2(pc, iv, { keySize: 256/32, hasher: CryptoJS.algo.SHA512, iterations: 1000 }).toString(CryptoJS.enc.Hex)
+}
